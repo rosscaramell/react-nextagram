05b490b61c9639d155d923e73567936483e6068d
rosscaramell <57162841+rosscaramell@users.noreply.github.com>
2020-02-10 17:01:12 +0800

    updated package lock

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.render(
